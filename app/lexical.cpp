1fb1b4c741ef2b5698c05df11f4e0ff054d9cbc8
nnddss <1076645646@qq.com>
2020-11-04 14:48:25 +0800

    11

--- app/lexical.cpp
+++ app/lexical.cpp
@@ -56,52 +56,53 @@ int main(int argc,char *argv[]){
 	
 	 fp = fopen(argv[1], "rb");
 	 while(fgets(sentence,1024,fp)){
+	 	printf("%s\n",sentence);
 //		while(~scanf("%s",&sentence)){
-		sta[0]='#';
-		top=0;//top terminal 
-		top1=1;// true top
-		int l=strlen(sentence);
-
-		sentence[l-2]='#';
-		
-		for(i=0;i<=l-2;){
-			if(gettype(sentence[i])==0){
-				printf("E\n");
-				return 0;
-			}
-			for(int j=top1-1;j>=0;j--){
-				if(gettype(sta[j])!=0&&gettype(sta[j])!=7){
-					top=j;
-					break;
-				}
-			}
-			if(top==0||(top!=0&&matrix[gettype(sta[top])][gettype(sentence[i])]==-1)){
-				if(sentence[i]=='#')
-					return 0;
-				sta[top1]=sentence[i];
-				top1++; 
-				printf("I%c\n",sentence[i]);
-				i++;
-			}
-			else if(matrix[gettype(sta[top])][gettype(sentence[i])]==1||matrix[gettype(sta[top])][gettype(sentence[i])]==0){
-				if(reduce()==1)
-					printf("R\n");
-				else {
-					printf("RE\n");
-					return 0;
-				}
-				
-			}
-			else if(matrix[gettype(sta[top])][gettype(sentence[i])]==2){
-				printf("E\n");
-				return 0;
-			}
-			
-		}
-		memset(sentence,0,1024);
-		memset(sta,0,1024);
-		top=0;
-		top1=0;
+//		sta[0]='#';
+//		top=0;//top terminal 
+//		top1=1;// true top
+//		int l=strlen(sentence);
+//
+//		sentence[l-2]='#';
+//		
+//		for(i=0;i<=l-2;){
+//			if(gettype(sentence[i])==0){
+//				printf("E\n");
+//				return 0;
+//			}
+//			for(int j=top1-1;j>=0;j--){
+//				if(gettype(sta[j])!=0&&gettype(sta[j])!=7){
+//					top=j;
+//					break;
+//				}
+//			}
+//			if(top==0||(top!=0&&matrix[gettype(sta[top])][gettype(sentence[i])]==-1)){
+//				if(sentence[i]=='#')
+//					return 0;
+//				sta[top1]=sentence[i];
+//				top1++; 
+//				printf("I%c\n",sentence[i]);
+//				i++;
+//			}
+//			else if(matrix[gettype(sta[top])][gettype(sentence[i])]==1||matrix[gettype(sta[top])][gettype(sentence[i])]==0){
+//				if(reduce()==1)
+//					printf("R\n");
+//				else {
+//					printf("RE\n");
+//					return 0;
+//				}
+//				
+//			}
+//			else if(matrix[gettype(sta[top])][gettype(sentence[i])]==2){
+//				printf("E\n");
+//				return 0;
+//			}
+//			
+//		}
+//		memset(sentence,0,1024);
+//		memset(sta,0,1024);
+//		top=0;
+//		top1=0;
 	} 
     fclose(fp);
     return 0;
